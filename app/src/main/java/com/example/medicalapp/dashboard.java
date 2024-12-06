c3c6ab5c3a3dab720941411850028193915d98f8
GabrielLangstaff117 <2212743@leedstrinity.ac.uk>
2024-12-06 07:57:40 +0000

    updated manifest, made a simple dashboard

--- app/src/main/java/com/example/medicalapp/dashboard.java
+++ app/src/main/java/com/example/medicalapp/dashboard.java
@@ -1,4 +1,12 @@
 package com.example.medicalapp;
 
-public class dashboard {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
 }
